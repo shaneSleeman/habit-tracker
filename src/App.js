f1a21837349f3c24cea8ec0fbe1ca88980892b40
shaneSleeman <shanesleeman1@gmail.com>
2022-11-22 12:12:13 +1100

    Add mui dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Dashboard from "./Dashboard";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
